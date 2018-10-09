e529b4ee1718b4a7ae98eaf9b5e8423712c79cb6
Karolina Gauvain <karolina.gauvain@gmail.com>
2018-10-09 23:25:30 +0200

    Define colors and squares variables

--- colorGame.js
+++ colorGame.js
@@ -1,1 +1,16 @@
+//Define variables
+let colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)",
+  ];
 
+const squares = document.querySelectorAll(".square");
+
+//Change the color of squares to the rgb colors
+for(let i=0; i < squares.length; i++){
+  squares[i].style.backgroundColor = colors[i];
+}
