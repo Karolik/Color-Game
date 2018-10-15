2ba3604b7112801086a30ceccfb9395d6eada81f
Karolina Gauvain <karolina.gauvain@gmail.com>
2018-10-15 15:34:31 +0200

    Add resetButton functionality

--- colorGame.js
+++ colorGame.js
@@ -15,6 +15,26 @@ const colorDisplay = document.querySelector("#colorDisplay");
 const messageDisplay = document.querySelector("#message");
 const h1 = document.querySelector("h1");
 const nav = document.querySelector("nav");
+const resetButton = document.querySelector(".reset");
+
+//Reset the game when pressed the resetButton
+resetButton.addEventListener("click", function(){
+  //Generate all new colors
+  colors = generateRandomColors(6);
+  //Pick a new random color from array
+  pickedColor = pickColor();
+  //Change colorDisplay to match picked color
+  colorDisplay.textContent = pickedColor;
+  //Change colors of squares
+  for(let i=0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+  }
+  //Set back the background of the heading to the default color
+  h1.style.backgroundColor = "#5C5CE7";
+  nav.style.color = "#5C5CE7";
+  //Change the text back to "New colors"
+  resetButton.textContent = "NEW COLORS";
+})
 
 //Set the RGB text in the header to the RGB of the selected color
 colorDisplay.textContent = pickedColor;
@@ -30,6 +50,7 @@ for(let i=0; i < squares.length; i++){
     //Compare color to picked color
     if(clickedColor === pickedColor){
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again?";  //Change the button text from "New colors" to "Play again?"
       changeColors(clickedColor);
       h1.style.backgroundColor = clickedColor;
       nav.style.color = clickedColor;
